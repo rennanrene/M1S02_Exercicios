331b0efca76024363fe1c95f4d0a5115598bfd03
Rennan Cavalcante <rennanrene@gmail.com>
2021-11-03 08:44:06 -0300

    Resolvando Ex 08

--- [M1S02] - Ex 8 - Ordenando o conteúdo de secções/script.js
+++ [M1S02] - Ex 8 - Ordenando o conteúdo de secções/script.js
@@ -1,10 +1,26 @@
 var textos = ["animal","carro","humano","prédio","cadeira","objeto"];
+var dicionario = [];
 
-var ordenados = textos.sort();
+for (var i=0; i < textos.length; i++){
+    let novoElemento = {
+        "palavra" : textos[i],
+        "tamanho" : textos[i].length
+    };
+    dicionario.push(novoElemento);
+};
+
+
+var ordenados = dicionario.sort(ordenarPorTamanho);
+
+function ordenarPorTamanho(a, b){
+    return a.tamanho- b.tamanho;
+    }
 
 for (var i=0; i < ordenados.length; i++){
     var novasection = document.createElement("section");
-    novasection.setAttribute("id", ordenados[i]);
+    var j = ordenados.length - 1 - i;
+    console.log(j);
+    novasection.setAttribute("id", ordenados[j]["palavra"]);
     document.body.appendChild(novasection);
-    novasection.innerHTML = ordenados[i];
-}
+    novasection.innerHTML = ordenados[j]["palavra"];
+};
